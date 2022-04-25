2206fb66324b3993fce6b299f364134d6b5f9dbf
Youngsik Yang <vacu9708@gmail.com>
2022-04-26 07:01:26 +0900

    Authentification upgraded

--- police server/server.js
+++ police server/server.js
@@ -15,31 +15,19 @@ backend.use('/node_modules/socket.io/client-dist', express.static(__dirname+'/no
 
 // Session setting
 backend.use(session({
-    //httpOnly: true,	//자바스크립트를 통해 세션 쿠키를 사용할 수 없도록 함
-    //secure: true,	//https 환경에서만 session 정보를 주고받도록처리
-    secret: 'secret key',	// Key for encryption
-    resave: false, //세션을 언제나 저장할지 설정함
-    saveUninitialized: true, //세션이 저장되기 전 uninitialized 상태로 미리 만들어 저장
+    //httpOnly: true, // Session information cannot be used through javascript
+    //secure: true,	// Session information can only be exchanged in https only
+    secret: 'secret key', // Key for encryption
+    resave: false,
+    saveUninitialized: true,
 }));
-
-socketio.on("connection", (socket)=>{
-    socket.join("observation room");
-
-	socket.on('streaming', (data) => {
-        //console.log(data)
-		socketio.in("observation room").emit('streaming', data);
-	});
-    socket.on('message from python', (message) => {
-		console.log(message);
-		socketio.in("observation room").emit('message from python', message);
-	});
-})
 
 // configuration of get request
 backend.get("/", (req, res) => {
     res.redirect('/public/Login.html')
 });
 backend.get("/Observation", (req, res) => {
+    console.log(req.session.user)
     if(req.session.user) // If logged in
         res.sendFile(__dirname+'/staff_only/Observation.html');
     else
@@ -51,6 +39,29 @@ backend.get("/Observation", (req, res) => {
 //     socketio.emit('message from python', req.body.message);
 // });
 
+let authorized_ips=[];
+socketio.on("connection", (socket)=>{
+    if(authorized_ips.findIndex((element)=>{return element==socket.conn.remoteAddress})!=-1){
+        socket.join("observation room");
+        console.log("("+socket.conn.remoteAddress+") connected");
+    }
+    else
+        socket.disconnect();
+
+    socket.on("disconnect", (reason)=>{
+        authorized_ips.splice(authorized_ips.findIndex((element)=>{element==socket.conn.remoteAddress}), 1)
+    })
+
+	socket.on('streaming', (data) => {
+        //console.log(data)
+		socketio.in("observation room").emit('streaming', data);
+	});
+    socket.on('message from python', (message) => {
+		console.log(message);
+		socketio.in("observation room").emit('message from python', message);
+	});
+})
+
 let user_ID = 'police'; let user_PW = '112';
 // Set what to do when a post request from '/process/login' arrives
 backend.post('/process/login', (request, response)=>{
@@ -58,11 +69,14 @@ backend.post('/process/login', (request, response)=>{
     const param_PW = request.body.password;
     
     if(param_ID == user_ID && param_PW == user_PW){ // Login successful
-        if(request.session.user == undefined)
+        if(request.session.user == undefined){
             request.session.user = {
                 id: param_ID,
                 authorized: true
             };
+            authorized_ips.push(request.ip);
+            //console.log(request.ip);
+        }
 
         response.send("<script>alert('Login successful'); location.href='/Observation';</script>");
         response.end();
